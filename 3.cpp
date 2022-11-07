693e516bd3a452a738166102ef09cb4a13b1e70d
ZlataProkurova <91047338+ZlataProkurova@users.noreply.github.com>
2022-11-07 17:12:45 +0300

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int a, b;
+	double c;
+	double S;
+	
+
+
+
+
+	std::cout << "Введите значение первого катета :" << std::endl;
+	std::cin >> a;
+	std::cout << "Введите значение второго катета катета :" << std::endl;
+	std::cin >> b;
+	c = pow(a, 2) + pow(b, 2);
+
+	std::cout << "Гипотенуза = " << c << std::endl;
+
+	S = 0.5 * a * b;
+		std::cout << " Площадь прямоугольного треугольника = " << S << std::endl;
+	return 0;
+}
