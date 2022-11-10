c35c3f782aed3072f9c8b6196a46bab94a218ebb
ZlataProkurova <91047338+ZlataProkurova@users.noreply.github.com>
2022-11-10 21:59:04 +0300

    Add files via upload

---
+++ ConsoleApplication66.cpp
@@ -0,0 +1,27 @@
+﻿
+
+#include <iostream>
+#include <math.h>
+
+float xtodec(float p, float f) {
+    return p*cos(f);
+
+}
+
+
+float ytodec(float p, float f) {
+    return p*sin(f);
+}
+
+int main()
+{
+    float x;
+    float y;
+    std::cout << "Vvedite polyarnie koordinati:\n";
+    std::cin >> x >> y;
+    std::cout << "Decartovie koord:" << xtodec(x, y) << " " << ytodec(x, y);
+    return 0;
+
+
+}
+
