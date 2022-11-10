ba61328181399fd44fc375cee8749b11a62574f7
ZlataProkurova <91047338+ZlataProkurova@users.noreply.github.com>
2022-11-10 20:13:56 +0300

    Add files via upload

---
+++ 7.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream> 
+#include <cmath> 
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "rus");
+	double a, b, c, d, x1, x2;
+	std::cout << "Введите a " << std::endl;
+	cin >> a;
+	std::cout << "Введите b " << std::endl;
+	cin >> b;
+	std::cout << "Введите c " << std::endl;
+	cin >> c;
+	d = pow(b, 2) - 4 * a * c;
+	if (d > 0) {
+		x1 = ((-b) + sqrt(d)) / (2 * a);
+		x2 = ((-b) - sqrt(d)) / (2 * a);
+		cout << "x1= " << x1 << std::endl;
+		cout << "x2= " << x2 << std::endl;
+	}
+	if (d == 0) {
+		x1 = -(b / (2 * a));
+		cout << "x1 =  " << x1 << std::endl;
+	}
+	if (d<0)
+		cout << "Уравнение не имеет корней";
+	
+}
+
+
+
+
