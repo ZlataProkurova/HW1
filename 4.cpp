e0ea7403347facfb28e2cc47e68d7e0e01e328a4
ZlataProkurova <91047338+ZlataProkurova@users.noreply.github.com>
2022-11-10 21:45:36 +0300

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -1,10 +1,10 @@
 
 #include <iostream>
-int summ(int x) {
+int summ(int x) { //создаем целочисленную функцию с параметром х
     int a = x / 100;
     int b = (x / 10) % 10;
     int c = x % 10;
-    return a + b + c;
+    return a + b + c;//возвращаем целочисленную сумму 
 }
 
 int main()
@@ -12,7 +12,7 @@ int main()
     int chislo;
     std::cout << "vvedite chislo\n";
     std::cin >> chislo;
-    std::cout << "Summa cifr v nem=" << summ(chislo);
+    std::cout << "Summa cifr v nem=" << summ(chislo); //выводим в консоль предложение и значение вызванной функции от аргумента, который был введен с клавиатуры
     return 0;
 
 }
